ce9493c716911866422646463abfeeb4e45c6d08
de isaac <deisaack@gmail.com>
2017-07-31 17:45:49 +0300

    update urls for unrestricted update

--- appraisal/urls.py
+++ appraisal/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.decorators.cache import never_cache
+
+from ckeditor_uploader import views as ck_views
 
 urlpatterns = [
+    url(r'^ckeditor/upload/', ck_views.upload, name='ckeditor_upload'),
+    url(r'^ckeditor/browse/', ck_views.browse, name='ckeditor_browse'),
     url(r'^admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^service/', include('service.urls', namespace='services')),
